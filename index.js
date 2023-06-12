da1771fe12db320dcf7e779b5f45d25124b04ef3
Dmitriev-Dmitrii <56238391+dmitriev-dmitrii@users.noreply.github.com>
2023-06-12 13:53:50 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,29 @@
-console.log('hui')
+// console.log('hui')
 
-alert ('navigator :' + window.navigator.userAgent ) 
-alert ('devicePixelRatio:' +  window.devicePixelRatio) 
+// alert ('navigator :' + window.navigator.userAgent ) 
+// alert ('devicePixelRatio:' +  window.devicePixelRatio) 
+
+
+function* visitCssRule(cssRule) {
+    // visit imported stylesheet
+    if (cssRule.type == cssRule.IMPORT_RULE)
+        yield* visitStyleSheet(cssRule.styleSheet);
+
+    // yield media rule
+    if (cssRule.type == cssRule.MEDIA_RULE)
+        yield cssRule;
+}
+
+function* visitStyleSheet(styleSheet) {
+    try {
+        // visit every rule in the stylesheet
+        var cssRules = styleSheet.cssRules;
+        for (var i = 0, cssRule; cssRule = cssRules[i]; i++)
+            yield* visitCssRule(cssRule);
+    } catch (ignored) {}
+}
+
+function* findAllMediaRules() {
+    // visit all stylesheets
+    var styleSheets = document.styleSheets;
+    for (var 
