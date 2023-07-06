99d6244b8afe0d7ebca27fe941a75f30891ae269
Dmitriev-Dmitrii <dmitriev.dmitrii@mail.com>
2023-07-07 00:49:55 +0300

    dates

--- index.js
+++ index.js
@@ -2,28 +2,3 @@
 
 // alert ('navigator :' + window.navigator.userAgent ) 
 // alert ('devicePixelRatio:' +  window.devicePixelRatio) 
-
-
-function* visitCssRule(cssRule) {
-    // visit imported stylesheet
-    if (cssRule.type == cssRule.IMPORT_RULE)
-        yield* visitStyleSheet(cssRule.styleSheet);
-
-    // yield media rule
-    if (cssRule.type == cssRule.MEDIA_RULE)
-        yield cssRule;
-}
-
-function* visitStyleSheet(styleSheet) {
-    try {
-        // visit every rule in the stylesheet
-        var cssRules = styleSheet.cssRules;
-        for (var i = 0, cssRule; cssRule = cssRules[i]; i++)
-            yield* visitCssRule(cssRule);
-    } catch (ignored) {}
-}
-
-function* findAllMediaRules() {
-    // visit all stylesheets
-    var styleSheets = document.styleSheets;
-    for (var 
