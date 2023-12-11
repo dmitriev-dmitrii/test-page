dfc751118de4d2ea9b3510dc8de30545232bedf3
dmitriev-dmitrii <dmitriev.dmitrii@mail.com>
2023-12-11 18:04:48 +0300

    styles

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
 import navBar from "./navBar.js";
-console.log( ('navigator :' + window.navigator.userAgent ));
-console.log( ('devicePixelRatio:' +  window.devicePixelRatio));
+
 navBar()
-
-function  a(abc) {
-
-    console.trace();
-}
-a()
-
-function name(params) {
-    a()
-}
-name()
--- navBar.js
+++ navBar.js
@@ -1,7 +1,29 @@
 
+const navSourceList =  [
+          {href:"index.html",text:"Home"},
+          {href:"dates.html",text:"Dates"},
+          {href:"flex-gap.html",text:"Flex Gap"},
+          {href:"canvas.html",text:"Canvas"},
+          {href:"reactive.html",text:"Reactive"},
+     ]
 
 export default function () {
+     
+   const liArr = navSourceList.map(({href,text})=> `<li><a href="${href}">${text}</a></li>`).join('')
 
-     document.querySelector('body')
+   const nav =   document.createElement('nav')
 
+
+   nav.innerHTML = (
+     `<ul> ${liArr} </ul>` )
+     
+   let header = document.querySelector('header')
+
+   if (header){
+     header.prepend(nav)
+     return
+   }
+   header = document.createElement('header')
+   header.append(nav)
+   document.querySelector('body').prepend(header)
 }
