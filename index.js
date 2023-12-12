48e6469810c627603f2581b2af92cd7505d5499c
dmitriev-dmitrii <dmitriev.dmitrii@mail.com>
2023-12-12 03:45:22 +0300

    test

--- index.js
+++ index.js
@@ -1,2 +1,46 @@
 import navBar from "./navBar.js";
 
+
+
+var createReactive = function (obj, onSet) {
+
+    obj = obj || {};
+    onSet = onSet || function () {};
+    var newObj = {};
+
+
+    Object.defineProperty(newObj, 'locals', {
+        enumerable: false,
+        value: {}
+    });
+
+    Object.keys(obj).forEach(function (key) {
+
+        Object.defineProperty(newObj, key, {
+            enumerable: true,
+            get: function () {
+                return this.locals[key];
+            },
+            set: function (newValue) {
+                this.locals[key] = newValue;
+                onSet(newObj);
+            }
+        });
+
+        newObj[key] = obj[key];
+    });
+
+    return newObj;
+};
+
+var render = function (obj) {
+    console.log('render');
+    console.log(obj);
+};
+
+var a = createReactive({
+        n: 42
+    }, render);
+
+a.n++
+
